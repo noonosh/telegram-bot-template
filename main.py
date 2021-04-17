2ee88da43e4fb8193525e1513ad0e349cd658fdb
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-04-17 17:00:14 +0500

    Initialize ConversationHandler
    Add some new comments for fresh users

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-
+# This is called callback. It is derived from the Handler
 def start(update, context):
     update.message.reply_text('Hello, World!')
 
@@ -19,8 +19,19 @@ def start(update, context):
 def main():
     updater = Updater(token=API_TOKEN)
     dispatcher = updater.dispatcher
-
+    
+    # Initialize Handler for getting into the conversation with the user
+    conversation = ConversationHandler(
+        entry_points=[],
+        states={},
+        fallbacks=[]
+    )
+    
+    # Handlers catch the updates and pass them into callbacks (this handler below sends the user's update into start callback)
+    # Callbacks also can be named as functions
     dispatcher.add_handler(CommandHandler('start', start))
-
+    
+    # Updater constantly checks for 'news' from Telegram server
+    # By default updater is set to run every 5 seconds
     updater.start_polling()
     updater.idle()
