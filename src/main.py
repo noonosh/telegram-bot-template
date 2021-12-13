070991474a1283e9e204ae353228d31a442a179e
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-12-13 19:40:23 +0000

    Set up main.py file

--- src/main.py
+++ src/main.py
@@ -1,38 +1,39 @@
-from telegram import *
-from telegram.ext import *
+import os
+import dotenv
 import logging
+from telegram.ext import (Updater,
+                          CommandHandler,
+                          ConversationHandler)
+from src.components import start
 
 
-# Enable logging
+dotenv.load_dotenv()
+DEBUG = os.environ.get('DEBUG', False) == 'True'
+
+
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    filename='logs.log' if not DEBUG else None,
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG if DEBUG else logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
-# This is called callback. It is derived from the Handler
-
-
-def start(update, context):
-    update.message.reply_text('Hello, World!')
-
 
 def main():
-    updater = Updater(token=API_TOKEN)
+    updater = Updater(os.environ['BOT_TOKEN'])
     dispatcher = updater.dispatcher
 
-    # Initialize Handler for getting into the conversation with the user
-    conversation = ConversationHandler(
-        entry_points=[],
+    main_conversation = ConversationHandler(
+        entry_points=[
+            CommandHandler('start', start.start)
+        ],
         states={},
         fallbacks=[]
     )
 
-    # Handlers catch the updates and pass them into callbacks (this handler below sends the user's update into start callback)
-    # Callbacks also can be named as functions
-    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(main_conversation)
 
-    # Updater constantly checks for 'news' from Telegram server
-    # By default updater is set to run every 5 seconds
     updater.start_polling()
     updater.idle()
