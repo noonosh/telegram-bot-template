72b0d40972e5e604182af723c9806537ffe0744e
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-16 14:14:43 +0100

    Test bot valid?

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,4 +1,15 @@
+import dotenv
+import os
+import requests
+
+dotenv.load_dotenv()
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '0')
 
 
-def test_start_callback():
-    pass
+def test_bot_valid():
+    if BOT_TOKEN == '0':
+        raise Exception('Invalid token')
+
+    res = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/getMe")
+    assert res.ok == True
